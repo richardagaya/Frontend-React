869dc382d2088118aaf069598cd5a91c2c1e46d6
richard agaya <richardagaya278@gmail.com>
2024-05-07 22:25:30 +0300

    new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-blue-700 text-white py-6">
             <div className="Container mx-auto text-center">
                 <p className="text-sm">2024 Your Website . All rights Reserved</p>
-                <div className="flex justify-center mt-4">
+                <div className="flex justify-center mt-4 ">
                     <a href="" className="mr-4 hover:text-gray-400">About</a>
                     <a href="" className="mr-4 hover:text-gray-400">Services</a>
                     <a href="" className="mr-4 hover:text-gray-400">Contact</a>
